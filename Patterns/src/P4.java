55973162c2194c993b8bb44c4315822ef793e502
Mayank <mayankanand2701@gmail.com>
2025-10-25 18:52:57 +0530

    Adding Code for Basic Patterns

---
+++ Patterns/src/P4.java
@@ -0,0 +1,25 @@
+/**
+ 	Code to Print the following Pattern :
+ 	1
+ 	22
+ 	333
+ 	4444
+ 	55555
+*/
+import java.util.Scanner;
+public class P4 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of terms = ");
+		int not=sc.nextInt();
+	    printPattern(not);
+	}
+	static void printPattern(int not)
+	{
+		for(int i=1;i<=not;i++)
+		{
+		for(int j=1;j<=i;j++) System.out.print(i+" ");
+		System.out.println();
+		}
+	}
+}
