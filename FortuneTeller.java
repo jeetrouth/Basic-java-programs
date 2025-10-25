3593a224f9a2bed84b128a82fad13a82eedcd00a
Spidy394 <79023855+Spidy394@users.noreply.github.com.>
2025-10-25 11:05:59 +0530

    fix filename typo

--- FortuneTeller.java
+++ FortuneTeller.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ForttuneTeller {
+public class FortuneTeller {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
